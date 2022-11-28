03a3bbccea7462ecb40c45be35584219e265ddec
Sergio Esteban Torres <sergio.smiling@gmail.com>
2022-11-28 10:37:03 -0500

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { ReactComponent as Logo } from '../public/logo.svg';
+
+const categories = [
+  {
+    name: 'Money Patterns', 
+    slug: 'money-patterns'
+  }, 
+  { name: 'Life Patterns', 
+    slug: 'life-patterns'
+  },
+]
+
+const Header = () => {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link href='/'>
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              Lifetiment
+             </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+              <Link key={category.slug} href={`/category/${category.slug}`}>
+                <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                  {category.name}
+                </span>
+              </Link>
+            ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,55 @@
+// import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services';
+
+export default function Home({ posts }) {
+ /*  const posts = [
+    { 
+      title: 'Life Patterns of Success',
+      excerpt: 'learn the most reliable and pure lessons from the greatest Successful people..'
+    },
+    { 
+      title: '5 Rules to manage your time as Stoics',
+      excerpt: 'The same type of problem, how can we ajust a greater amount of tasks in the same amount of life-hours...?'
+    },
+    {
+      title: 'Money An Ubiquitous Necessity',
+      excerpt: 'which one is a better strategy, save more or gain more? maybe both but...'
+    },
+  ] */
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Lifetiment Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12 w-auto'>
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard key={post.title} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+// how to fetch data using getStaticProps in next.js
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+}
